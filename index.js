04ba42bc9c807016f5c08a0a5c6df10a167487fd
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-10-05 22:36:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const require = createRequire(__dirname);
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('The Shawaza\nBot', {
+say('7rB 2leX\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot Made by Sayed Shawaza`, {
+say(`Bot Made by 7rB 2leX`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
